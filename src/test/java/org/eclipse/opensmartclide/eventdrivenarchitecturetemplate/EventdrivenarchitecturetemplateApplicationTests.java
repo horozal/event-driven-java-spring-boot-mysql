d5a2ca0a1165208f77e89029eaf6bbcdc11bd1b7
Feryal Fulya Horozal <horozal@atb-bremen.de>
2023-02-22 02:06:03 +0100

    add project structure

---
+++ src/test/java/org/eclipse/opensmartclide/eventdrivenarchitecturetemplate/EventdrivenarchitecturetemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eclipse.opensmartclide.eventdrivenarchitecturetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventdrivenarchitecturetemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
